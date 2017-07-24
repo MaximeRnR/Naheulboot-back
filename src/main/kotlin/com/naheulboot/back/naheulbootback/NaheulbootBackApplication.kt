940d3813ddaef06745074dd4be3f0d7742b1b98d
Maxime Reynier <mreynier@excilys.com>
2017-07-24 13:48:09 +0200

    Spring Initialzr

---
+++ src/main/kotlin/com/naheulboot/back/naheulbootback/NaheulbootBackApplication.kt
@@ -0,0 +1,11 @@
+package com.naheulboot.back.naheulbootback
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class NaheulbootBackApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(NaheulbootBackApplication::class.java, *args)
+}
